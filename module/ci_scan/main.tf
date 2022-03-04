4073d49f914e8359177f5dafdf8b80e9d11cefd2
Tan Quach <tquach@joor.com>
2022-03-03 20:34:29 -0500

    fix: use key

--- module/ci_scan/main.tf
+++ module/ci_scan/main.tf
@@ -66,7 +66,7 @@ resource "aws_autoscaling_group" "ci_appliance_asg" {
       }
     ]
     content {
-      key                 = tag.key
+      key                 = tag.value["key"]
       value               = tag.value["value"]
       propagate_at_launch = true
     }
